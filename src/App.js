df2e8aa6a48029bdc3d2cbb89ed5e4b9d58c387d
Sunghyun Lee <98dltjdgus@naver.com>
2020-05-24 20:15:40 +0900

    Add components(Navigation, Logo, ImageLinkForm, Rank), update App and etc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Particles from "react-particles-js";
+import Navigation from "./component/Navigation/Navigation";
+import Logo from "./component/Logo/Logo";
+import Rank from "./component/Rank/Rank";
+import ImageLinkForm from "./component/ImageLinkForm/ImageLinkForm";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 90,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+  },
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* 
+      <FaceRecognition />} */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./ImageLinkForm.css";
+
+const ImageLinkForm = () => {
+  return (
+    <div>
+      <p className="f3">
+        {"This Magic Brain will detect faces in your pictures. Git it a try."}
+      </p>
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input className="f4 pa2 w-70 center" type="text" />
+          <button className="w-30 grow f4 link pv2 dib white bg-green">
+            Detect
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageLinkForm;
+
+/*
+  size of 4(f4), 'padding' if 2(pa2), width going to be 70%,
+  going to be 'centered' because width 70%.
+
+  w-30 : width 30%
+  grow : grow when you hover over it
+  f4 : size of 4
+  link : it's a link
+  ...기타등등
+*/
