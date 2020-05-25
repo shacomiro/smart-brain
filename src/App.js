25f070e425bada002e628f15a1b9afce5a82ebb9
Sunghyun Lee <98dltjdgus@naver.com>
2020-05-25 22:24:59 +0900

    Add new components(FaceRecognition, Register, Signin) and update components(ImageLinkForm, Navigation), container(App)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Clarifai from "clarifai";
 import Particles from "react-particles-js";
 import Navigation from "./component/Navigation/Navigation";
 import Logo from "./component/Logo/Logo";
 import Rank from "./component/Rank/Rank";
 import ImageLinkForm from "./component/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./component/FaceRecognition/FaceRecognition";
+import Signin from "./component/Signin/Signin";
+import Register from "./component/Register/Register";
 import "./App.css";
 
 const particlesOptions = {
@@ -18,17 +22,107 @@ const particlesOptions = {
   },
 };
 
+const app = new Clarifai.App({
+  apiKey: "aa4e641e96464328af75e13196a5c6bf",
+});
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {},
+      route: "signin",
+      isSignedIn: false,
+    };
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.querySelector("#inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  };
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => {
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // 신규 사용법?
+    // app.models
+    //   .initModel({
+    //     id: Clarifai.COLOR_MODEL,
+    //   })
+    //   .then((generalModel) => {
+    //     return generalModel.predict(
+    //       "https://samples.clarifai.com/face-det.jpg"
+    //     );
+    //   })
+    //   .then((response) => {
+    //     console.log(response);
+    //     var concepts = response["outputs"][0]["data"]["concepts"];
+    //   });
+  };
+
+  onRouteChange = (route) => {
+    if (route == "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
+
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm />
-        {/* 
-      <FaceRecognition />} */}
+        <Navigation
+          onRouteChange={this.onRouteChange}
+          isSignedIn={isSignedIn}
+        />
+        {this.state.route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition imageUrl={imageUrl} box={box} />
+          </div>
+        ) : route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
