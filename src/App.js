ffd4633027ed99292366101e3287958c124872e6
Sunghyun Lee <98dltjdgus@naver.com>
2020-06-19 23:43:48 +0900

    Deploy on heroku

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends React.Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
 
-    fetch('http://localhost:3001/imageurl', {
+    fetch('https://radiant-caverns-40633.herokuapp.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -89,7 +89,7 @@ class App extends React.Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch('http://localhost:3001/image', {
+          fetch('https://radiant-caverns-40633.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
